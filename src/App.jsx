6dc6ae7d89d7545a3ea4329d7226a018229391ec
tanvirtbf <tanver018765@gmail.com>
2024-04-04 11:53:12 +0600

    complete App.jsx ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="p-4 bg-pink-600 text-3xl">Project</h1>
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeBtn  */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* Card  */}</div>
+      </div>
+    </div>
   );
 }
 
